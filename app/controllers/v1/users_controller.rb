9f8623513e6cbdf11ce5e58c33586645a6467ff9
sebabelmar <si.belmar@gmail.com>
2015-09-24 19:18:38 -0300

    Initial comit

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,29 @@
+module V1
+  class V1::UsersController < ApplicationController
+    skip_before_action :authenticate_user_from_token!, only: [:create]
+
+    # POST /v1/users
+    # Creates an user
+    def create
+      @user = User.new user_params
+
+      puts "*******"
+        p params
+        p @user
+      puts "*******"
+
+
+      if @user.save
+        render json: @user, serializer: V1::SessionSerializer, root: nil
+      else
+        render json: { error: t('user_create_error') }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
+  end
+end
