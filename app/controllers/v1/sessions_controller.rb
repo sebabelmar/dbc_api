9b6c4588614d1543fdca27f684a06b7ebd2dc648
sebabelmar <si.belmar@gmail.com>
2015-09-25 01:07:37 -0300

    fancy modules

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,26 +1,24 @@
-module V1
-  class SessionsController < ApplicationController
-    skip_before_action :authenticate_user_from_token!
+class V1::SessionsController < ApplicationController
+  skip_before_action :authenticate_user_from_token!
 
-    # POST /v1/login
-    def create
-      @user = User.find_for_database_authentication(email: params[:username])
-      return invalid_login_attempt unless @user
+  # POST /v1/login
+  def create
+    @user = User.find_for_database_authentication(email: params[:username])
+    return invalid_login_attempt unless @user
 
-      if @user.valid_password?(params[:password])
-        sign_in :user, @user
-        render json: @user, serializer: SessionSerializer, root: nil
-      else
-        invalid_login_attempt
-      end
+    if @user.valid_password?(params[:password])
+      sign_in :user, @user
+      render json: @user, serializer: SessionSerializer, root: nil
+    else
+      invalid_login_attempt
     end
-
-    private
-
-    def invalid_login_attempt
-      warden.custom_failure!
-      render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
-    end
-
   end
+
+  private
+
+  def invalid_login_attempt
+    warden.custom_failure!
+    render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
+  end
+
 end
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,23 +1,21 @@
-module V1
-  class V1::UsersController < ApplicationController
-    skip_before_action :authenticate_user_from_token!, only: [:create]
+class V1::UsersController < ApplicationController
+  skip_before_action :authenticate_user_from_token!, only: [:create]
 
-    # POST /v1/users
-    # Creates an user
-    def create
-      @user = User.new user_params
+  # POST /v1/users
+  # Creates an user
+  def create
+    @user = User.new user_params
 
-      if @user.save
-        render json: @user, serializer: V1::SessionSerializer, root: nil
-      else
-        render json: { error: t('user_create_error') }, status: :unprocessable_entity
-      end
+    if @user.save
+      render json: @user, serializer: V1::SessionSerializer, root: nil
+    else
+      render json: { error: t('user_create_error') }, status: :unprocessable_entity
     end
+  end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:email, :username, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 end
